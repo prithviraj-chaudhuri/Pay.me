66a6a1b4de7cc804aa6665322b33adb6ff530881
Prithviraj Chaudhuri <p.chaudhuri1993@gmail.com>
2024-03-24 19:01:20 -0400

    Updated styling for splitting

--- ui/src/app/Split.js
+++ ui/src/app/Split.js
@@ -1,39 +1,59 @@
 // import React, { useRef, useState, useEffect } from 'react';
 // import { checkDeviceType } from '../common/helper';
+import './Split.css';
 
 function Split() {
 
 
   return (
-    <div className="ios-style ios-container">
-        <h1 className="mt-5 mb-4 text-center ios-title">Pay.me</h1>
-        <div class="mt-4 ios-camera-container">
-            <div class="card">
-            <div class="card-body">
-                <h5 class="card-title">Select Your Options</h5>
-                <form>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="option1" value="option1"></input>
-                    <label class="form-check-label" for="option1">Option 1</label>
+    <div class="row rem-space">
+        <div class="col-sm-12 center-div">
+            <div class="card full-width">
+                <div class="card-header">
+                    Oil
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="option2" value="option2"></input>
-                    <label class="form-check-label" for="option2">Option 2</label>
+                <div class="card-body">
+                    <h5 class="card-title">$15.00</h5>
+                    <p class="card-text">Select people to split this cost with</p>
+                    <div class="card person-card" >
+                        <div class="card-body row">
+                            <div class="col-6">Prithviraj</div>
+                            <div class="col-6">
+                                <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off"></input>
+                                <label class="btn btn-outline-primary" for="btn-check-outlined">Add</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card person-card" >
+                        <div class="card-body row">
+                            <div class="col-6">Taylor</div>
+                            <div class="col-6">
+                                <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off"></input>
+                                <label class="btn btn-outline-primary" for="btn-check-outlined">Add</label>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="card person-card" >
+                        <div class="card-body row">
+                            <div class="col-6">Srishti</div>
+                            <div class="col-6">
+                                <input type="checkbox" class="btn-check" id="btn-check-outlined" autocomplete="off"></input>
+                                <label class="btn btn-outline-primary" for="btn-check-outlined">Add</label>
+                            </div>
+                        </div>
+                    </div>
+                    <br/>
+                    <div>
+                        <div class="row">
+                            <div class="col-6">
+                                <button className="btn ios-button">Previous</button>
+                            </div>
+                            <div class="col-6">
+                                <button className="btn ios-button">Next</button>
+                            </div>
+                        </div>
+                    </div>
                 </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="option3" value="option3"></input>
-                    <label class="form-check-label" for="option3">Option 3</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="option4" value="option4"></input>
-                    <label class="form-check-label" for="option4">Option 4</label>
-                </div>
-                <div class="form-check">
-                    <input class="form-check-input" type="checkbox" id="option5" value="option5"></input>
-                    <label class="form-check-label" for="option5">Option 5</label>
-                </div>
-                </form>
-            </div>
             </div>
         </div>
     </div>
